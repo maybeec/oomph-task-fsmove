f4b14c0ef607270e434f19bdb4b6310de3db243b
Steffen Holzer <steffen.holzer@capgemini.com>
2016-07-22 11:20:28 +0200

    cleaned gh-branch and added update data

--- task-fsmove/src/com/github/maybeec/oomph/task/fsmove/core/FSMoveUtil.java
+++
@@ -1,9 +0,0 @@
-package com.github.maybeec.oomph.task.fsmove.core;
-
-import com.github.maybeec.oomph.task.fsmove.core.exception.FSMoveException;
-
-public interface FSMoveUtil {
-	
-	void move(String source, String destination) throws FSMoveException;
-	
-}
